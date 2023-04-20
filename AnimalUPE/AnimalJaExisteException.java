c64151de73562e0783df23ac10b51ec411ed4b19
Júlio Cesar <juliocesar27072004@outlook.com>
2023-04-20 08:41:04 -0300

    Delete AnimalUPE directory

--- AnimalUPE/AnimalJaExisteException.java
+++
@@ -1,7 +0,0 @@
-package AnimalUPE;
-
-public class AnimalJaExisteException extends Exception {
-    public AnimalJaExisteException() { super(); }
-
-    public AnimalJaExisteException(String msg) { super(msg); }
-}
