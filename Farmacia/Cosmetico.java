7776d3c347076b8483bb066de382fda0724e0e04
Júlio Cesar <juliocesar27072004@outlook.com>
2023-04-20 08:43:01 -0300

    Delete Farmacia directory

--- Farmacia/Cosmetico.java
+++
@@ -1,8 +0,0 @@
-package Farmacia;
-
-public class Cosmetico extends Product {
-    public Cosmetico(String descriçao, int estoque, Double preço, Fornecedor fornecedor) {
-        super(descriçao, estoque, preço, fornecedor);
-        this.setDescontoMax(0.2);
-    }
-}
